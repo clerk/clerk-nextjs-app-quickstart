dcf2c03d01988ba0f593c177a8680e8a771eaaf0
LekoArts <lekoarts@gmail.com>
2023-11-29 08:56:11 +0100

    fix: Use h-screen on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen">
       <UserButton afterSignOutUrl="/" />
     </div>
   )
