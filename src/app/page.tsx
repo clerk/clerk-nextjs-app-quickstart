b45a8704a32426177df2c0ee86043571698d7966
Roy Anger <roy@royanger.com>
2024-01-17 01:53:29 -0500

    Updated for v5 to match updated Quickstart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="h-screen">
-      <UserButton afterSignOutUrl="/" />
+      <SignedOut>
+        <SignInButton />
+        <p>This content is public. Only signed out users can see this.</p>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <p>This content is private. Only signed in users can see this.</p>
+      </SignedIn>
     </div>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default clerkMiddleware()
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
