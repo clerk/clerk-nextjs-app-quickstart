9b8b855666da48021ffce74c78132f53c1e71666
Domitrius <domitriusaclark@gmail.com>
2024-07-15 10:10:43 -0400

    removed auth call & testing button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import { auth } from "@clerk/nextjs/server";
-
 export default function Home() {
-  const { userId } = auth();
-  console.log({ userId });
-  return <div></div>;
+  return <div>Home Page</div>;
 }
