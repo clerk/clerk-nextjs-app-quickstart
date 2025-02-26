edf7acd600722b0a6cfce432b5a6d83f4b72cb50
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-02-26 13:57:47 -0500

    update dependecies, including tailwind v4

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
               app/page.tsx
             </code>
             .
