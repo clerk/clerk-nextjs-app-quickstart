498644ec262319ac179f02ab1bbbff0abd1cf62f
Jacek <jacek@clerk.dev>
2025-12-05 07:14:23 -0600

    feat: Upgrade next to latest

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server'
 
 // This Middleware does not protect any routes by default.
 // See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
-export default clerkMiddleware()
+export const proxy = clerkMiddleware()
 
 export const config = {
   matcher: [
